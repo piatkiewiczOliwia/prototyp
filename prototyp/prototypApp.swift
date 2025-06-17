6f1cfd5f48a9dc7ae956490c5362ca48632d243f
Oliwia Piątkiewicz <piatkiewicz.oliwia0606@gmail.com>
2025-06-17 15:10:29 +0200

    Initial Commit

---
+++ prototyp/prototypApp.swift
@@ -0,0 +1,17 @@
+//
+//  prototypApp.swift
+//  prototyp
+//
+//  Created by Oliwia Piątkiewicz on 17/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct prototypApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
