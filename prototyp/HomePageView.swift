5272fc283dbd077d26c5ded7d628d29caef8a34e
Oliwia Piątkiewicz <piatkiewicz.oliwia0606@gmail.com>
2025-06-19 13:36:16 +0200

    Other half of the home view (uncleaned)

--- prototyp/HomePageView.swift
+++ prototyp/HomePageView.swift
@@ -21,18 +21,12 @@ struct HomePageView: View {
                     TopBarHomePageButton(icon: "list.bullet.rectangle.portrait", text: "Lists")
 
                 }
-                HStack(alignment: .firstTextBaseline){
-                    Image(systemName: "magnifyingglass")
-                        .font(.title2)
-                        .padding(.leading, 15)
-                        .padding(.top, 8)
-                    TextField("Search...", text: $searchBarText)
-                }
-                .padding(.bottom, 5)
+                
+                searchBar
                 Divider()
                     .frame(width: UIScreen.main.bounds.width*0.9)
                 
-                
+                //top filters
                 HStack(alignment: .firstTextBaseline){
                     Spacer()
                     Text("Top Filters ⭐️")
@@ -54,6 +48,7 @@ struct HomePageView: View {
                         TopFiltersScrollViewItemView(emote: "🍔")
 
                     }
+                    .frame(height: 115)
                 }
                 .frame(width: UIScreen.main.bounds.width*0.9)
                 .offset(x: 0, y: -20)
@@ -70,15 +65,104 @@ struct HomePageView: View {
                         endPoint: .trailing
                     )
                 )
+                .padding(.bottom, -10)
+            
+                //quiz
+                HStack(alignment: .firstTextBaseline){
+                    Text("Take a quick quiz!")
+                        .font(.title3)
+                        .fontWeight(.medium)
+                        .padding(.leading, 15)
+                    Spacer()
+                }
+                .padding(.top, 0)
+                .padding(.bottom, 0)
+                
+                Button {
+                
+                } label: {
+                    ZStack(alignment: .bottomTrailing) {
+                        Image("")
+                            .resizable()
+                            .scaledToFill()
+                            .foregroundColor(.black)
+                            .frame(width: UIScreen.main.bounds.width*0.9, height: UIScreen.main.bounds.height*0.15)
+                            .background(Color.black.opacity(0.08))
+                            .cornerRadius(15)
+                        .overlay(.white.opacity(0.6))
+                        
+                        Text("Tell us your cravings..")
+                                    .font(.headline)
+                                    .fontWeight(.semibold)
+                                    .foregroundColor(.black.opacity(0.3))
+                                    .italic()
+                                    .padding()
+                        
+                    }
+                        
+                }
+                
+                //hot today
+                HStack(alignment: .firstTextBaseline){
+                    Spacer()
+                    Text("Hot today 🔥")
+                        .font(.title3)
+                        .fontWeight(.medium)
+                        .padding(.trailing, 40)
+    
+                }
+                .padding(.top, 20)
+                .padding(.bottom, 0)
+                
+                let imageNames = ["first image", "second image", "third image"]
+                
+                TabView {
+                            ForEach(imageNames, id: \.self) { name in
+                                Text(name)
+                                    .frame(width: UIScreen.main.bounds.width * 0.9,
+                                           height: UIScreen.main.bounds.height * 0.15)
+                                    .background(.black.opacity(0.1))
+                                    .clipped()
+                                    
+                                    .overlay(Color.white.opacity(0.6))
+                            }
+                        }
+                        .tabViewStyle(.page)
+                        .frame(width: UIScreen.main.bounds.width * 0.9,
+                               height: UIScreen.main.bounds.height * 0.15)
+                        .clipShape(RoundedRectangle(cornerRadius: 15))
+                        .padding(.bottom)
+                
+                //nav bar
+            
+                Divider()
+                    .frame(width: UIScreen.main.bounds.width*0.9)
+                    .padding(.bottom)
+        
+                
+                HStack(spacing: 45){
+                    Image(systemName: "house")
+                    Image(systemName: "magnifyingglass")
+                    Image(systemName: "plus.app")
+                    Image(systemName: "list.bullet.rectangle.portrait")
+                    Image(systemName: "person")
+                }
+                .font(.title2)
+                
+
                 
                     
             
                 Spacer()
                 
+                
+                
             }
+            //frame na vstack
         }
         
     }
+        
 }
 
 #Preview {
@@ -97,5 +181,16 @@ extension HomePageView {
                 .padding(.trailing, 15)
                 .font(.title2)
         }.padding(.top)
+    }
+    
+    private var searchBar: some View{
+        HStack(alignment: .firstTextBaseline){
+            Image(systemName: "magnifyingglass")
+                .font(.title2)
+                .padding(.leading, 15)
+                .padding(.top, 8)
+            TextField("Search...", text: $searchBarText)
+        }
+        .padding(.bottom, 5)
     }
 }
